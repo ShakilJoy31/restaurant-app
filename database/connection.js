c68f6354fc20429f4a445924cf790725be7d0533
ShakilJoy31 <joy31a@gmail.com>
2023-03-07 17:59:09 +0600

    env var problem

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectMongo = async () =>{
     try{
-        const {connection} = await mongoose.connect(process.env.MONGO_URI); 
+        const {connection} = await mongoose.connect('mongodb+srv://restaurantShakil:HxjwpDy4A4U9rzl3@cluster0.6fewfet.mongodb.net/?retryWrites=true&w=majority'); 
         if(connection.readyState === 1){
             console.log('Database is connected.'); 
         }
